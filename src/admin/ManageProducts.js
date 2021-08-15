5b9e7d8e749dac893014bd1c6894d7109398ee20
Aniket Chavan <aniketchavan_20816@aitpune.edu.in>
2021-08-16 00:19:58 +0530

    Changes in manage products

--- src/admin/ManageProducts.js
+++ src/admin/ManageProducts.js
@@ -44,7 +44,7 @@ const ManageProducts = () => {
       </Link>
       <div className="row">
         <div className="col-12">
-          <h2 className="text-center text-white my-3">Total 3 products</h2>
+          <h2 className="text-center text-white my-3">Total {products.length} Products</h2>
           {products.map((product, index) => {
             return (
               <div key={index} className="row text-center mb-2 ">
